ed9d574de4ed885ca1a1adfade8c5f3c24cdcaef
ThorbenJensen <jensen.thorben@gmail.com>
2017-04-18 22:30:11 +0200

    adding extraction of wifi strengths on linux

---
+++ src/wifi_location.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 18 22:04:51 2017
+
+@author: thor
+"""
+
+import os
+import subprocess
+import re
+
+#%%
+
+# linux
+a = subprocess.check_output(['nmcli', 'dev', 'wifi'])
+
+#%%
+
+a = str(a)
+b = str.split(a, '\\n')
+
+#%% 
+
+r1 = [ re.findall('[0-9A-Z]{2}:[\wA-Z\:]+', c) for c in b ][1:-1]
+r2 = [ re.findall('MB/s\s+(\w+)\s', c) for c in b ][1:-1]
+
+r3 = [ i for [i] in r1]
+r4 = [ i for [i] in r2]
+
+#%%
+
+# TODO: store observations to SQLITE
