ee7ebe337964764e7b6e6245b3877498980f368f
Thorben Jensen <thorben.jensen@de.ey.com>
2017-04-19 19:28:42 +0200

    etl done. next: get clean feature matrix

--- src/windows.py
+++ src/windows.py
@@ -11,10 +11,9 @@ import sqlite3
 import datetime
 
 
-def add_timestamp_to_df(df):
-    """add timestamp to DataFrame."""
-    df['timestamp'] = datetime.datetime.utcnow()
-    return df
+# locations that can be logged
+locations = ['traveling', 'living_room', 'kitchen', 'bedroom', 'bathroom']
+database_path = '../data/wifi.sqlite'
 
 
 def signals_windows():
@@ -28,16 +27,53 @@ def signals_windows():
     e2 = [i for [i] in e]
     f2 = [i for [i] in f]
     df = pd.DataFrame({'bssid': e2, 'signal': f2})
-    df = add_timestamp_to_df(df)
     return df
 
 
-def write_signals_to_db(db='flights.db', df=None):
+def write_signals_to_db(db=database_path, df=None):
     """Write wifi signals to database."""
     con = sqlite3.connect(db)
-    df.to_sql(name='test', con=con, flavor='sqlite', if_exists='append',
-              index=False)
+    df.to_sql(name='windows', con=con, flavor='sqlite', if_exists='append', index=False)
     con.close()
+    
 
-df = signals_windows()
-write_signals_to_db('../data/wifi.sqlite', df)
+def get_location():
+    """Get location from user (options in script parameters)."""
+    print('Where are you located?')
+    for i in range(0, len(locations)):
+        print("%i - %s" % (i, locations[i]))
+    k = int(input('location: '))
+    return locations[k]
+ 
+
+def log_signals(db=database_path, location=None):
+    """Log current wifi signals."""
+    df = signals_windows()
+    df['timestamp'] = datetime.datetime.utcnow()
+    df['location'] = location    
+    write_signals_to_db(db, df)
+
+
+def log_location():
+    """Ask for location and log it, together with current wifi signals."""
+    location = get_location()
+    print('Logging location "%s" ... ' % location)
+    log_signals(location=location)
+
+def read_log_from_db(db=database_path):
+    """Read signal log as dataframe."""
+    con = sqlite3.connect(db)
+    df = pd.read_sql('SELECT * FROM windows', con=con)
+    con.close()
+    return df
+    
+def get_feature_matrix():
+    """Create feature matrix from signal log"""
+    df = read_log_from_db()
+    # TODO
+    return df
+    
+
+print(get_feature_matrix())
+    
+    # log_location()
