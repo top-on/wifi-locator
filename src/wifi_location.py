3c0bf70a89831637b4e821105f117233f8b67dd5
Thorben Jensen <thorben.jensen@de.ey.com>
2017-04-19 07:46:45 +0200

    pep8 formatting

--- src/wifi_location.py
+++ src/wifi_location.py
@@ -1,32 +1,22 @@
-# -*- coding: utf-8 -*-
 """
-Created on Tue Apr 18 22:04:51 2017
+Get wifi signals on linux.
 
 @author: thor
 """
 
-import os
 import subprocess
 import re
-
-#%%
 
 # linux
 a = subprocess.check_output(['nmcli', 'dev', 'wifi'])
 
-#%%
-
 a = str(a)
 b = str.split(a, '\\n')
 
-#%% 
+r1 = [re.findall('[0-9A-Z]{2}:[\wA-Z\:]+', c) for c in b][1:-1]
+r2 = [re.findall('MB/s\s+(\w+)\s', c) for c in b][1:-1]
 
-r1 = [ re.findall('[0-9A-Z]{2}:[\wA-Z\:]+', c) for c in b ][1:-1]
-r2 = [ re.findall('MB/s\s+(\w+)\s', c) for c in b ][1:-1]
-
-r3 = [ i for [i] in r1]
-r4 = [ i for [i] in r2]
-
-#%%
+r3 = [i for [i] in r1]
+r4 = [i for [i] in r2]
 
 # TODO: store observations to SQLITE
