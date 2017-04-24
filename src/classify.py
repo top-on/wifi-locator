eb8592ed928e49655833e22b8815380bbfe1f474
Thorben Jensen <thorben.jensen@de.ey.com>
2017-04-24 20:49:54 +0200

    set up detailed model comparison; next: predict location for given signal.

--- src/classify.py
+++ src/classify.py
@@ -5,41 +5,47 @@ Created on Mon Apr 24 07:15:05 2017
 @author: DETJENS2
 """
 
-from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble.weight_boosting import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
 import numpy as np
-
 from windows import get_feature_matrix
 from windows import get_labels
 
-
-
-def evaluate_model(model, x, y):
-    model.fit(x, y.values.ravel())
-    y_pred = model.predict(x)
-    labels = list(set(y.values.ravel()))
-    print('Random Forests:')
-    print('Labels: %s' % labels)
-    print('Confusion_matrix (reusing data):')
-    print(confusion_matrix(y.values.ravel(), y_pred, labels=labels))
-    print('CV f1_micro (not reusing data): %s' 
-          % np.mean(cross_val_score(model, x, y.values.ravel(), cv=5, scoring='f1_micro')))
-
-
+# historial data
 x = get_feature_matrix()
 y = get_labels()
-#print(x)
-#print(y)
+# define models
+classifiers = {'KNN': KNeighborsClassifier(n_neighbors=3, algorithm='auto',
+                                           metric='braycurtis'),
+               'RandForest': RandomForestClassifier(n_estimators=80, n_jobs=1),
+               #'SVM': SVC(gamma=2, C=1),
+               #'linear SVM': SVC(kernel="linear", C=0.025),
+               'DecisionTree': DecisionTreeClassifier(max_depth=5),
+               'AdaBoost': AdaBoostClassifier(n_estimators=80, learning_rate=0.4),
+               #'Naive Bayes': GaussianNB(),
+               }
 
-# KNN
-model = KNeighborsClassifier(n_neighbors=3, algorithm='auto', metric='braycurtis') 
-evaluate_model(model, x, y)
 
-# decision trees
-model = RandomForestClassifier(n_estimators=50)
-evaluate_model(model, x, y)
+def evaluate_model(model_name, model, x, y):
+    """Evaluate model accuracy via cross validation."""
+    print('%s:' % model_name)
+    model.fit(x, y.values.ravel())
+    print('CV f1_micro (not reusing data): %s' % np.mean(cross_val_score(model, x, 
+          y.values.ravel(), cv=5, scoring='f1_micro')))
+
     
+def predict():
+    """Predict current location, including classifier voting."""
+    # combine classifiers
+    #vc = VotingClassifier(classifiers.items(), n_jobs=1)
+    #evaluate_model('VotingClassifier', vc, x, y)    
+    pass
     
-    
+           
+if __name__ == '__main__':
+    # evaluate models
+    for name in classifiers.keys():
+        evaluate_model(name, classifiers[name], x, y)        
--- src/windows.py
+++ src/windows.py
@@ -91,7 +91,6 @@ def get_labels():
     df = df.sort_index()
     return df
 
-
-# log_location()
-
-
+if __name__ == '__main__':
+    # log location
+    log_location()
